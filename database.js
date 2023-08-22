a120961c585f242d0850a0babf8b892f5d6510b4
Shubhechchha Karmacharya <80193330+shubhechchhakarmacharya@users.noreply.github.com>
2023-08-22 07:11:05 +0545

    Update database.js

--- database.js
+++ database.js
@@ -1,18 +1,15 @@
 const { Sequelize } = require('sequelize');
 
-// Replace these values with your actual PostgreSQL database configuration
-const DB_NAME = 'PersonalDiary';
-const DB_USER = 'postgres';
-const DB_PASSWORD = 'postgres123';
-const DB_HOST = 'localhost';
-const DB_PORT = 5432; 
+const DB_URL = '';
 
-// Create Sequelize instance
-const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-  host: DB_HOST,
-  port: DB_PORT,
-  dialect: 'postgres', 
-  logging: false, 
+const sequelize = new Sequelize(DB_URL,{
+  dialect: 'postgres',
+  logging: 'false',
+  dialectOptions:{
+    ssl:{
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
-
-module.exports = sequelize;
+module.exports = sequlize;
