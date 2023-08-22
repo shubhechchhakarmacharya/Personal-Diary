97241b02bd807679c34e0d20fc947728dce36785
Shubhechchha Karmacharya <80193330+shubhechchhakarmacharya@users.noreply.github.com>
2023-08-22 07:12:38 +0545

    Done

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const DB_URL = '';
+const DB_URL = 'postgres://shubhechchha:JyrzPhQvdrtuJWzZfnypiGSUznxwOnAp@dpg-cji0rpb37aks73f80hj0-a.oregon-postgres.render.com/diaryrecords';
 
 const sequelize = new Sequelize(DB_URL,{
   dialect: 'postgres',
